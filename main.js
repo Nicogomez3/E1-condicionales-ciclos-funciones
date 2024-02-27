7d11f54d02b34f79193af07d046d6f9694ed696c
Nicogomez3 <Nicogmz2014@hotmail.com>
2024-02-27 16:59:40 -0300

    E1- condicionales, ciclos y funciones

---
+++ main.js
@@ -0,0 +1,101 @@
+//EJERCICIO 1
+
+// function parImpar(num) {
+//     if(num % 2 === 0) {
+//         console.log(`El numero ${num} es par`)
+//     } else {
+//         console.log(`El numero ${num} es impar`)
+//     }
+// }
+
+// parImpar(9);
+
+//EJERCICIO 2
+
+// function menorMayor(num1, num2) {
+//     if(num1 > num2) {
+//         console.log(`El numero ${num1} es mayor que ${num2}`)
+//     } else if (num1 < num2) {
+//         console.log(`El numero ${num2} es mayor que ${num1}`)
+//     } else {
+//         console.log(`Los numeros son iguales`)
+//     }
+// }
+
+// menorMayor(10, 10);
+
+//EJERCICIO 3
+
+// function multiplo (numero) {
+//     if(numero % 5 === 0) {
+//         console.log(`el numero ${numero} es multiplo de 5`)
+//     } else {
+//         console.log(`El numero ${numero} no es multiplo de 5`)
+//     }
+// }
+
+//multiplo(25)
+
+//EJERCICIO 4
+
+// function numeros (numero) {
+//     for(let i = 0; i <= numero; i++) {
+//         if(i <= numero) {
+//             console.log(i)
+//         }
+//     }
+// }
+
+// numeros(5)
+
+//EJERCICIO 5
+
+// function palabraRepetitiva(numero, palabra) {
+    
+//     for(let i = 1; i <= numero; i++) {
+//          console.log(palabra);
+//     };  
+// }
+
+// palabraRepetitiva (5,"Amelia")
+
+//EJERCICIO 6
+
+// const bocaJuniors = [ 1, "Romero", 17, "Advincula", 6, "Rojo", 4, "Figal", 10, "Cavani", 9, "Benedetto", 16, "Merentiel"]
+
+// function equipoBoca(bocaJuniors) {
+//     for(let jugadores of bocaJuniors) {
+//         console.log(jugadores);
+//     }
+// }
+
+// equipoBoca(bocaJuniors)
+
+//EJERCICIO 7 
+
+// const numeros = [1, 2 ,3 ,4 ,5 ,6 ,7 ,8 ,9 ,10]
+
+// function adios5taPosicion(numeros) {
+//     for(let i = 1; i <= numeros.length; i++) {
+//         if(numeros[i] === 5) {
+//             continue;
+//         }
+//         console.log(i)
+//     }
+// }
+
+// adios5taPosicion(numeros)
+
+//EJERCICIO 8 
+
+// const numerosArr = [1 ,2 ,3 ,4 ,5 ,6 ,7 ,8 ,9 ,10]
+
+// function multiplicar(numerosArr, numero) {
+//     let i = 0
+//     while(i <= 10) {
+//         console.log(i * numero)
+//         i++
+//     }
+// }
+
+//  multiplicar(numerosArr, 2)
